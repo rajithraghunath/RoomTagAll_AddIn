a214e4408d109db3d7a6d029d8c85c67e496298c
rajithraghunath <rajithraghunath0@gmail.com>
2025-04-10 18:10:24 +0530

    Add files via upload

---
+++ Class1 - RoomsTag_TagAllRooms.cs
@@ -0,0 +1,188 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomsTag
+{
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class TagAllRooms : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            try
+            {
+                Document doc = commandData.Application.ActiveUIDocument.Document;
+
+                Transaction tran = new Transaction(doc, "Rooms №");
+                tran.Start();
+                // Create a new sample class - create a new instance of class data
+                CreateNumber numb = new CreateNumber(commandData);
+
+                tran.Commit();
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                // If there are something wrong, give error information and return failed
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+
+    public class CreateNumber
+    {
+        UIApplication modRevit;
+
+        List<Room> modRooms = new List<Room>();    // The collection stores all the rooms in the project - a list to store all rooms in the project
+        List<RoomTag> modRoomsTag = new List<RoomTag>(); // The collection stores all the tags (tags) of the room in the project - a list to store all room tags
+        List<Room> modRoomsWithTag = new List<Room>();   // a list to store all rooms with tag
+        List<Room> modRoomsWithoutTag = new List<Room>(); // a list to store all rooms without tag
+
+        public ReadOnlyCollection<RoomTag> RoomTags
+        {
+            get
+            {
+                return new ReadOnlyCollection<RoomTag>(modRoomsTag);
+            }
+        }
+
+        public ReadOnlyCollection<Room> RoomsWithoutTag
+        {
+            get
+            {
+                return new ReadOnlyCollection<Room>(modRoomsWithoutTag);
+            }
+        }
+
+        public CreateNumber(ExternalCommandData commandData)
+        {
+            modRevit = commandData.Application;
+
+            // get all the rooms and room tags in the project
+            GetAllRoomsAndTags();
+
+            // find out the rooms that without room tag
+            ClassifyRooms();
+
+            // Brand Creation
+            CreateTags();
+        }
+
+        // find out the rooms that without room tag
+        private void ClassifyRooms()
+        {
+            modRoomsWithoutTag.Clear();
+            modRoomsWithTag.Clear();
+
+            // copy the all the elements in list Rooms to list RoomsWithoutTag
+            modRoomsWithoutTag.AddRange(modRooms);
+
+            // get the room id from room tag via room property
+            // if find the room id in list RoomWithoutTag,
+            // add it to the list RoomWithTag and delete it from list RoomWithoutTag
+            foreach (RoomTag tmpTag in modRoomsTag)
+            {
+                ElementId idValue = tmpTag.Room.Id;
+                modRoomsWithTag.Add(tmpTag.Room);
+
+                // search the id for list RoomWithoutTag
+                foreach (Room tmpRoom in modRooms)
+                {
+                    if (idValue == tmpRoom.Id)
+                    {
+                        modRoomsWithoutTag.Remove(tmpRoom);
+                    }
+                }
+            }
+        }
+
+        // get all the rooms and room tags in the project
+        private void GetAllRoomsAndTags()
+        {
+            // get the active document 
+            Document document = modRevit.ActiveUIDocument.Document;
+            RoomFilter roomFilter = new RoomFilter();
+            RoomTagFilter roomTagFilter = new RoomTagFilter();
+            LogicalOrFilter orFilter = new LogicalOrFilter(roomFilter, roomTagFilter);
+
+            FilteredElementIterator elementIterator = new FilteredElementCollector(document)
+                .WherePasses(orFilter)
+                .GetElementIterator();
+            elementIterator.Reset();
+
+            // try to find all the rooms and room tags in the project and add to the list
+            while (elementIterator.MoveNext())
+            {
+                object obj = elementIterator.Current;
+
+                // find the rooms, skip those rooms which don't locate at Level yet.
+                Room tmpRoom = obj as Room;
+                if (null != tmpRoom && null != document.GetElement(tmpRoom.LevelId))
+                {
+                    modRooms.Add(tmpRoom);
+                    continue;
+                }
+
+                // find the room tags
+                RoomTag tmpTag = obj as RoomTag;
+                if (null != tmpTag)
+                {
+                    modRoomsTag.Add(tmpTag);
+                    continue;
+                }
+            }
+        }
+
+        public void CreateTags()
+        {
+            try
+            {
+                foreach (Room tmpRoom in modRoomsWithoutTag)
+                {
+                    // get the location point of the room
+                    LocationPoint locPoint = tmpRoom.Location as LocationPoint;
+                    if (null == locPoint)
+                    {
+                        String roomId = "Room Id:  " + tmpRoom.Id.ToString();
+                        String errMsg = roomId + "\r\nError creating a label (room tag),  " +
+                                                   "I can't get the location! Create a room";
+                        throw new Exception(errMsg);
+                    }
+
+                    // create a instance of Autodesk.Revit.DB.UV class
+                    UV point = new UV(locPoint.Point.X, locPoint.Point.Y);
+
+                    //create room tag
+                    RoomTag tmpTag;
+                    tmpTag = modRevit.ActiveUIDocument.Document.Create.NewRoomTag(new LinkElementId(tmpRoom.Id), point, null);
+                    if (null != tmpTag)
+                    {
+                        modRoomsTag.Add(tmpTag);
+                    }
+                }
+
+                // classify rooms
+                ClassifyRooms();
+
+                // display a message box
+                TaskDialog.Show("Revit", "Room tags have been successfully completed!");
+            }
+            catch (Exception exception)
+            {
+                TaskDialog.Show("Revit", exception.Message);
+            }
+        }
+    }
+}
---
+++ Class1 - rep can not be parsed .cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+
+[Transaction(TransactionMode.Manual)]
+public class TagAllRooms : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+        Document doc = uiDoc.Document;
+
+        try
+        {
+            using (Transaction trans = new Transaction(doc, "Tag All Rooms Including Linked Models"))
+            {
+                trans.Start();
+
+                int tagCount = 0;
+
+                // Get all floor plan views (non-template)
+                var viewPlans = new FilteredElementCollector(doc)
+                    .OfClass(typeof(ViewPlan))
+                    .Cast<ViewPlan>()
+                    .Where(v => v.ViewType == ViewType.FloorPlan && !v.IsTemplate)
+                    .ToList();
+
+                // Map LevelId to a corresponding ViewPlan
+                var levelViewMap = viewPlans
+                    .GroupBy(v => v.GenLevel.Id)
+                    .ToDictionary(g => g.Key, g => g.First());
+
+                // Get room tag symbol
+                var roomTagType = new FilteredElementCollector(doc)
+                    .OfClass(typeof(FamilySymbol))
+                    .OfCategory(BuiltInCategory.OST_RoomTags)
+                    .Cast<FamilySymbol>()
+                    .FirstOrDefault();
+
+                if (roomTagType == null)
+                {
+                    TaskDialog.Show("Error", "No room tag family loaded.");
+                    return Result.Failed;
+                }
+
+                if (!roomTagType.IsActive)
+                {
+                    roomTagType.Activate();
+                    doc.Regenerate();
+                }
+
+                //// Use a local function instead of a lambda for tagging a room
+                //void TagRoom(Room room, XYZ point, ElementId levelId)
+                //{
+                //    if (levelViewMap.TryGetValue(levelId, out ViewPlan view))
+                //    {
+                //        UV uv = new UV(point.X, point.Y);
+                //        doc.Create.NewRoomTag(new LinkElementId(room.Id), uv, view.Id);
+                //        tagCount++;
+                //    }
+                //}
+
+                // Helper method: tag a room
+                Action<Room, XYZ, ElementId> TagRoom = (room, point, levelId) =>
+                {
+                    if (levelViewMap.TryGetValue(levelId, out ViewPlan view))
+                    {
+                        UV uv = new UV(point.X, point.Y);
+                        doc.Create.NewRoomTag(new LinkElementId(room.Id), uv, view.Id);
+                        tagCount++;
+                    }
+                };
+
+                // ➤ Step 1: Tag rooms in the host model
+                var hostRooms = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_Rooms)
+                    .WhereElementIsNotElementType()
+                    .Cast<Room>()
+                    .Where(r => r.Area > 0 && r.Location is LocationPoint)
+                    .ToList();
+
+                foreach (Room room in hostRooms)
+                {
+                    XYZ point = (room.Location as LocationPoint).Point;
+                    TagRoom(room, point, room.LevelId);
+                }
+
+                // ➤ Step 2: Tag rooms in linked models
+                var linkInstances = new FilteredElementCollector(doc)
+                    .OfClass(typeof(RevitLinkInstance))
+                    .Cast<RevitLinkInstance>()
+                    .ToList();
+
+                foreach (RevitLinkInstance linkInstance in linkInstances)
+                {
+                    Document linkedDoc = linkInstance.GetLinkDocument();
+                    if (linkedDoc == null) continue;
+
+                    Transform linkTransform = linkInstance.GetTransform();
+
+                    var linkedRooms = new FilteredElementCollector(linkedDoc)
+                        .OfCategory(BuiltInCategory.OST_Rooms)
+                        .WhereElementIsNotElementType()
+                        .Cast<Room>()
+                        .Where(r => r.Area > 0 && r.Location is LocationPoint)
+                        .ToList();
+
+                    foreach (Room room in linkedRooms)
+                    {
+                        LocationPoint location = room.Location as LocationPoint;
+                        if (location == null) continue;
+
+                        XYZ linkedPt = location.Point;
+                        XYZ hostPt = linkTransform.OfPoint(linkedPt);
+
+                        // Choose a view in the host document (e.g., based on room.LevelId)
+                        if (levelViewMap.TryGetValue(room.LevelId, out ViewPlan view))
+                        {
+                            Reference linkedRoomRef = Reference.ParseFromStableRepresentation(doc, linkInstance.GetLinkDocument().GetElement(room.Id).UniqueId);
+
+                            //Reference linkedRoomRef = Reference.ParseFromStableRepresentation(linkedDoc, linkInstance.GetLinkDocument().GetElement(room.Id).UniqueId);
+
+                            //Reference linkedRoomRef = linkInstance.GetLinkElementReference(room.Id);
+                            if (linkedRoomRef == null) continue; // fail safe
+
+                            IndependentTag tag = IndependentTag.Create(
+                                doc,
+                                view.Id,
+                                linkedRoomRef,
+                                false, // isAttached
+                                TagMode.TM_ADDBY_CATEGORY,
+                                TagOrientation.Horizontal,
+                                hostPt
+                            );
+
+                            if (tag != null) tagCount++;
+                        }
+                    }
+                }
+
+
+
+
+                trans.Commit();
+                TaskDialog.Show("Room Tagger", $"Room tags placed: {tagCount}");
+                return Result.Succeeded;
+            }
+        }
+        catch (Exception ex)
+        {
+            message = ex.Message;
+            return Result.Failed;
+        }
+    }
+}
